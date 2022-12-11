8a1ea61aaa40aa6fa579d6a7c56a8194ef67a053
JamaiaB <114833468+JamaiaB@users.noreply.github.com>
2022-12-11 17:53:49 -0600

    updates

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# ReadMe-Generator
+# ReadMe Generator
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+- [Description](#description) 
+- [Installation](#installation) 
+- [Usage](#usage) 
+- [License](#license)
+
+## Description
+This project allows developers to generate their own readme's.
+
+## Installation
+github
+
+## Usage
+Other developers will be able to use my code to generate theor own readme's.
+
+
+## License
--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generatemarkdown');
+// TODO: Create an array of questions for user input
+const questions = [];
 
-inquirer.
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    const markdown = generateMarkdown(data);
+
+    fs.writeFile(fileName, markdown, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.
     prompt([
         {
             type: 'input',
             message: 'What is the name of your project?',
-            name: 'pname'
+            name: 'title'
         },
         {
             type: 'input',
@@ -23,4 +38,29 @@ inquirer.
             message: 'How will your project be used?',
             name: 'usage'
         },
-    ]);
+        {
+            type: 'list',
+            message: 'What license will this project have?',
+            name: 'license',
+            choices: ['MIT', 'ISC', 'APACHE', 'none'],
+        }
+    ]) .then((answers) => {
+        writeToFile('readme.md', answers)
+    }
+    )
+      
+}
+
+// Function call to initialize app
+init();
+
+
+
+    // .then((answers) => {
+    //     const readmePageContent = generateReadMe(answers);
+        
+    //     fs.writeFile('readme.md', htmlPageContent, (err) =>
+    //     err ? console.log(err) : console.log('Successfully created index.html!')
+    //   );    
+    // });
+    
